97fcebc1f3b8856b10885e28bfc07d417394a477
mguidoti <marcus.guidoti@gmail.com>
2020-11-12 22:52:37 -0300

    feat: Add final transformations and save pkl

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ from source.connect_tb import get_data
 from source.name_classifier import classify
 from source.combine_tb import combine
 
-
-
+from syno2pandas import splitCol2Rows
 
 
 if __name__ == "__main__":
@@ -20,15 +19,32 @@ if __name__ == "__main__":
   )
   
   original_taxon_list = [
-      "Pterovianaida duckensis",
-      "Pterovianaida melchiori",
-      "Rhinolophus",
-      "Rhinolophys"
+      "Rhinolophus"
   ]
 
   taxon_list = classify(original_taxon_list)
 
   df = combine(taxon_list)
   
-  print(df.shape)
-  # Send the DF to your functions here, Dario
+  # Attempt to split accession numbers
+  try:
+    df_split = splitCol2Rows(df)
+    logging.info('Accession Numbers successfully splited!')
+  except:
+    logging.error('Problem while spliting accession numbers.')
+  
+  # Attempt to remove version digit from accession numbers
+  try:
+    df_split['AccessionNumber'] = df_split['AccessionNumber'].str.replace(
+                                                                r'\.+\d', '')
+    logging.info('Version digit correctly removed from all accession numbers!')
+  except:
+    logging.error('Problem while removing version digit from accession numbers.')
+  
+  # Attempt to save pickle file
+  try: 
+    df_split.to_pickle('data.pkl')
+    logging.info('Pickle file successfully saved!')
+    
+  except:
+    logging.error('Problem saving pickle file.')
